5819bc4f3531e9c991b8a28f10698d1d04f17872
Your Name <forever.alone92@gmail.com>
2017-09-07 11:00:06 +0700

    display search result: success

--- index.js
+++ index.js
@@ -17,7 +17,22 @@ app.get('/api/imagesearch/:query', (req, res) => {
   const params = req.params.query;
   const search_url = api_url.concat(params);
   saveToDB(params, req, res);
-  request(search_url).pipe(res);
+  // add body to callback args will fuck up your response
+  request.get(search_url, (err, response) => {
+    var results = JSON.parse(response.body).items;
+    var images = results.map((val) => {
+      var og_url = val.pagemap.metatags[0]['og:image'];
+      var snippet = val.title;
+      var thumbnail = val.pagemap.cse_thumbnail !== undefined ? val.pagemap.cse_thumbnail[0].src : "no thumbnail";
+      var context = val.link;
+      return  { "url": og_url,
+                "snippet": snippet,
+                "thumbnail": thumbnail,
+                "context": context
+              }
+    });
+    res.send(images);
+  });
 });
 
 app.listen(port);
